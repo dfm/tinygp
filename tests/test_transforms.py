22373d3d0bb427e754dce499ef91411881736e81
Dan F-M <foreman.mackey@gmail.com>
2022-01-24 13:41:48 -0500

    more words in multivariate tutorial

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,31 +1,46 @@
 # -*- coding: utf-8 -*-
 # mypy: ignore-errors
 
-import jax
 import jax.numpy as jnp
 import numpy as np
-import pytest
 
-from tinygp import GaussianProcess, kernels, transforms
+from tinygp import kernels, transforms
 
 
-def test_affine():
+def test_linear():
     kernel0 = kernels.Matern32(4.5)
-    kernel1 = transforms.Affine(4.5, kernels.Matern32())
-    kernel2 = transforms.Affine(4.5 ** 2, kernels.Matern32(), variance=True)
+    kernel1 = transforms.Linear(1 / 4.5 ** 2, kernels.Matern32())
     np.testing.assert_allclose(
         kernel0.evaluate(0.5, 0.1), kernel1.evaluate(0.5, 0.1)
     )
+
+
+def test_multivariate_linear():
+    kernel0 = kernels.Matern32(4.5)
+    kernel1 = kernels.Matern32(jnp.full(3, 4.5))
+    kernel2 = transforms.Linear(jnp.full(3, 1 / 4.5 ** 2), kernels.Matern32())
     np.testing.assert_allclose(
-        kernel0.evaluate(0.5, 0.1), kernel2.evaluate(0.5, 0.1)
+        kernel0.evaluate(jnp.full(3, 0.5), jnp.full(3, 0.1)),
+        kernel1.evaluate(jnp.full(3, 0.5), jnp.full(3, 0.1)),
+    )
+    np.testing.assert_allclose(
+        kernel0.evaluate(jnp.full(3, 0.5), jnp.full(3, 0.1)),
+        kernel2.evaluate(jnp.full(3, 0.5), jnp.full(3, 0.1)),
     )
 
 
-def test_multivariate_affine():
+def test_cholesky():
+    kernel0 = kernels.Matern32(4.5)
+    kernel1 = transforms.Cholesky(4.5, kernels.Matern32())
+    np.testing.assert_allclose(
+        kernel0.evaluate(0.5, 0.1), kernel1.evaluate(0.5, 0.1)
+    )
+
+
+def test_multivariate_cholesky():
     kernel0 = kernels.Matern32(4.5)
     kernel1 = kernels.Matern32(jnp.full(3, 4.5))
-    kernel2 = transforms.Affine(jnp.full(3, 4.5), kernels.Matern32())
-    kernel3 = transforms.Affine(jnp.diag(jnp.full(3, 4.5)), kernels.Matern32())
+    kernel2 = transforms.Cholesky(jnp.full(3, 4.5), kernels.Matern32())
     np.testing.assert_allclose(
         kernel0.evaluate(jnp.full(3, 0.5), jnp.full(3, 0.1)),
         kernel1.evaluate(jnp.full(3, 0.5), jnp.full(3, 0.1)),
